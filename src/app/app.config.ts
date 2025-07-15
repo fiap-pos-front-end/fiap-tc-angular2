bd8bda6423b398c5474b7dad4ed07897f8628436
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-07-15 20:02:47 -0300

    fix: authInterceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,21 +4,19 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import Aura from '@primeng/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { provideHttpClient } from '@angular/common/http';
 
 registerLocaleData(localePt);
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(),
     provideAnimationsAsync(),
     providePrimeNG(),
     { provide: LOCALE_ID, useValue: 'pt-BR' },
