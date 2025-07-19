8c3e1a358e8bbad82cb80357ace313f06eaa9d63
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-07-19 11:26:17 -0300

    refactor: remove auth interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { registerLocaleData } from '@angular/common';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import localePt from '@angular/common/locales/pt';
 import {
   ApplicationConfig,
@@ -10,14 +10,13 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
-import { authInterceptor } from './shared/interceptors/auth.interceptor';
 
 registerLocaleData(localePt);
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(),
     provideAnimationsAsync(),
     providePrimeNG(),
     { provide: LOCALE_ID, useValue: 'pt-BR' },
